76fea05c8a6f01a3002b5ebebb4d8723f4ecf285
EnigmaCurry <ryan@enigmacurry.com>
2025-01-22 16:23:06 -0700

    add example secrets.h

---
+++ embedded/esp32/mqtt-bridge/morse-mqtt-bridge/secrets.h
@@ -0,0 +1,33 @@
+#define SECRET_HOST_NAME "morse-bridge"
+#define SECRET_WIFI_SSID "xxxxxx"
+#define SECRET_WIFI_PASS "xxxxxx"
+#define SECRET_MQTT_SERVER "mqtts://mqtt.example.com:8883"
+#define SECRET_MQTT_TOPIC_ROOT "morse-bridge"
+
+
+const char* SECRET_TLS_KEY =
+  "-----BEGIN EC PRIVATE KEY-----\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "-----END EC PRIVATE KEY-----\n";
+
+const char* SECRET_TLS_CERT =
+  "-----BEGIN CERTIFICATE-----\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "-----END CERTIFICATE-----\n"
+  "-----BEGIN CERTIFICATE-----\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "-----END CERTIFICATE-----\n";
+
+const char* SECRET_TLS_CA_CERT =
+  "-----BEGIN CERTIFICATE-----\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+  "-----END CERTIFICATE-----\n";
+
